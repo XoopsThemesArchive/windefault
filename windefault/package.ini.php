e7e780da85da6fd434ab6b8ca9f4d843ea237759
mambax7 <mambax7@gmail.com>
2015-05-05 09:36:44 -0400

    Original files

---
+++ windefault/package.ini.php
@@ -0,0 +1,16 @@
+<?php
+/**
+
+[Manifesto]
+Name="Default Theme-Win(Drag and Drop and Resizing) Version"
+Url="http://xoopscube.org and http://www.wanisys.net"
+Version="2.00"
+
+[Theme]
+Author=XOOPS Cube Project Team and Wanikoo and Authors of rico and prototype
+ScreenShot="screenshot.png"
+Description="Default theme-Win(Drag and Drop and Resizing) Version of XOOPS 2.0 JP"
+W3C=NG
+
+*/
+?>
